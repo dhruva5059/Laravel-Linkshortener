b0afb761e6d27cbebf54be0f97021d90891cc2fe
dhruva5059 <dhruva.5059@gmail.com>
2016-02-26 20:16:55 -0500

    Added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <html>
-
+@if(isset($shorturl))
+<p>{!!$shorturl!!}</p>
+@endif
 <form action="createShortLink" method="post">
 <input type="url" id="url" name="url"></input>
 <button type="submit">Submit</button>
